31e8a3aab2bef2f5ad8bd34398ea981ef7bdba18
Jordon-py <c.jordon@icloud.com>
2025-02-20 16:12:59 -0800

    Styling for Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 // src/App.jsx
-import { useContext } from 'react'; // comented
-import { Routes, Route } from 'react-router';
+import { useContext } from 'react'; // Removed useState since it's not used
+import { Routes, Route, useLocation } from 'react-router';
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Dashboard from './components/Dashboard/Dashboard';
 import Landing from './components/Landing/Landing';
-
 import { UserContext } from './contexts/UserContext';
-const App = () => {
- 
-  const { user } = useContext(UserContext)
 
+const App = () => {
+  const { user } = useContext(UserContext);
+  const location = useLocation();
+  const showNavBar = location.pathname !== '/';
+  
   return (
     <>
-      <NavBar />
+      {showNavBar && <NavBar />}
       <Routes>
-        <Route path='/' element={user ? <Dashboard /> : <Landing /> } />
+        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/sign-in' element={<SignInForm />} />
-      </Routes> 
+      </Routes>
     </>
   );
 };
-
+  
 export default App;
 
 // comment
