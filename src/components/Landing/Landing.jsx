206a6f6293bc046d05ef79f954b4d195249577e2
Jordon-py <c.jordon@icloud.com>
2025-02-23 22:48:31 -0800

    Enhance Landing.css: button text styling and hover state improvements

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,16 +1,28 @@
-// src/components/Landing.jsx 
-import './Landing.css'  
-const Landing = () => {
-    return (
-      <main>
-        <h1><em> STACKED </em></h1>
-        <p>Sign up now, or sign in to see your super secret dashboard!</p>
-        <a href="/sign-in"><button className="Login" type="button"> Sign-in</button></a>
-        <a href="/sign-up"><button className="Register" type="button"> Register</button></a>
-      </main>
-    );
-  };
-  
-  export default Landing;
-  
-  
+import './Landing.css';  
+
+function Landing ({ User }) {
+  return (
+    <main>
+      <h1 className="zoomIn"><em> STACKED </em></h1>
+      <p>Sign up now, or sign in to see your super secret dashboard!</p>
+      <div className="button-container">
+        {!User ? (
+          <>
+            <a href="/sign-in">
+              <button className="Login" type="button">Sign-in</button>
+            </a>
+            <a href="/sign-up">
+              <button className="Register" type="button">Register</button>
+            </a>
+          </>
+        ) : (
+          // if User exists
+          <p>Welcome back!</p>
+        )}
+      </div>
+    </main>
+  );
+}
+
+export default Landing;
+
